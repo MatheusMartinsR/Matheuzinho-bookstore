ee5dc3e089af558bf40ae1bae79e18762e5d3ad7
MatheusMartinsR <martinsmatheusrh08@gmail.com>
2023-09-22 20:54:15 -0300

    changes

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,87 +1,42 @@
 package br.com.fiap;
 
-import br.com.fiap.domain.entity.Author;
-import br.com.fiap.domain.entity.Book;
-import br.com.fiap.domain.entity.PessoaFisica;
-import br.com.fiap.domain.entity.PessoaJuridica;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import com.sun.net.httpserver.HttpServer;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.net.URI;
+import java.util.Map;
 
 public class Main {
 
+    private static final String BASE_URI = "http://localhost/";
+
+    private static HttpServer StartServer() {
+
+        final ResourceConfig rc = new ResourceConfig()
+                .packages( "br.com.fiap.domain.resources");
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory( "oracle", getProperties() );
-        EntityManager manager = factory.createEntityManager();
+        final HttpServer server = StartServer();
+        System.out.println(
+                String.format("Bookstore app started with endpoints available" +
+                        "as %s%nHit Ctrl-C to stop it....", BASE_URI)
+        );
 
-        var holding = new PessoaJuridica();
-        var bene = new PessoaFisica();
-        var vinicius = new PessoaFisica();
-
-        holding.setCnpj( UUID.randomUUID().toString() )
-                .setId( null )
-                .setNome( "Holding Benezinho SA" )
-                .setNascimento( LocalDate.now().minusYears( 5 ) );
-
-        bene.setCpf(UUID.randomUUID().toString() )
-                .setId( null )
-                .setNome( "Benefrancis do Nascimento" )
-                .setNascimento( LocalDate.of( 1977, 3, 8 ) );
-
-        vinicius.setCpf( UUID.randomUUID().toString() )
-                .setId( null )
-                .setNome( "Vinicius Cruzeiro Barbosa" )
-                .setNascimento( LocalDate.now().minusYears( 17 ) );
-
-        var authorBene = new Author();
-        authorBene.setPessoa( bene );
-
-        var authorVinicius = new Author();
-        authorVinicius.setPessoa( vinicius );
-
-        var livro = new Book();
-        livro.setISBN( UUID.randomUUID().toString() )
-                .setName( "Memórias de Benezinho" )
-                .setLaunch( LocalDateTime.now() )
-                .addAuthor( authorBene )
-                .addAuthor( authorVinicius );
-
-        manager.getTransaction().begin();
-        manager.persist( holding );
-        manager.persist( livro );
-        manager.getTransaction().commit();
-
-        System.out.println( holding );
-        System.out.println( livro );
-
-        manager.close();
-        factory.close();
-
+        try {
+            System.in.read();
+            server.stop();
+        }catch (IDException e){
+            throw new RuntimeException(e);
+        }
     }
+
 
     private static Map<String, Object> getProperties() {
-        Map<String, String> env = System.getenv();
-        Map<String, Object> properties = new HashMap<>();
 
-        for (String chave : env.keySet()) {
-            if (chave.contains( "USER_FIAP" )) {
-                properties.put( "jakarta.persistence.jdbc.user",  env.get( chave ) );
-            }
-            if (chave.contains( "PASSWORD_FIAP" )) {
-                properties.put( "jakarta.persistence.jdbc.password",  env.get( chave ) );
-            }
-            // Outras configurações de propriedade ....
-        }
-        return properties;
+
     }
-
-
 }
